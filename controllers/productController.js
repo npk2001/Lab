ab9e189470e55c061233367609320a9115e201a8
npk2001 <91059332+npk2001@users.noreply.github.com>
2021-11-26 08:08:35 +0000

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,8 +10,7 @@ const getProductByID = (request, response) => {
   const catalogServices = require("../services/productServices");
   let reference = request.params.reference;
   catalogServices.searchIDService(reference, function (err, rows) {
-    response.json(rows);
-    response.end();
+  response.render("articles", { product: rows });
   });
 };
 
